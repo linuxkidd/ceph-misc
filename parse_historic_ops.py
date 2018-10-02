df7ca6de9e0c34612b427bcbc3ba961b8aac8f0f
Michael J. Kidd <lk@lk-skull.rv>
2018-10-02 14:44:05 -0600

    Changed sort order of 'events' per Op in parse_historic_ops.py to be by completion time of the event

--- parse_historic_ops.py
+++ parse_historic_ops.py
@@ -19,6 +19,8 @@ for op in obj["Ops"]:
         utc_time = datetime.strptime(event["time"], "%Y-%m-%d %H:%M:%S.%f")
         gmtime = (utc_time - datetime(1970, 1, 1)).total_seconds()
         if(event["event"] not in timespent):
+            if(lastepoch==0):
+                lastepoch=gmtime
             timespent[event["event"]]={ 'delta':(gmtime-lastepoch), 'lastepoch': gmtime, 'lastdt':event["time"] }
         else:
             timespent[event["event"]]['delta']+=gmtime-timespent[event["event"]]["lastepoch"]
@@ -26,5 +28,5 @@ for op in obj["Ops"]:
             timespent[event["event"]]["lastdt"]=event["time"]
         lastepoch=gmtime
     print("{0} {1}".format(firstdt,op["description"]))
-    for event, edata in reversed(sorted(timespent.iteritems(), key=lambda (k,v): (v["delta"],k))):
+    for event, edata in sorted(timespent.iteritems(), key=lambda (k,v): (v["lastepoch"],k)):
         print("\t{0:8.4f} {1:s} {2:s}".format(edata["delta"],edata["lastdt"],event))
